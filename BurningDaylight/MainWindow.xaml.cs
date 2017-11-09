1de268c708261cbd1f21792c86f8156a83e0f6a6
Robert Jørgensgaard Engdahl <robert.j.engdahl@gmail.com>
2017-11-10 00:11:18 +0100

    Initial commit

---
+++ BurningDaylight/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using BurningDaylight.Properties;
+using Hardcodet.Wpf.TaskbarNotification;
+using YouTrackSharp;
+
+namespace BurningDaylight
+{
+    public partial class MainWindow
+    {
+        private UsernamePasswordConnection _youTrackConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ResetYouTrackConnection();
+
+            MonitorTimeTracking();
+        }
+
+        private async Task MonitorTimeTracking()
+        {
+
+            for (;;)
+            {
+                var issueService = _youTrackConnection?.CreateIssuesService();
+
+                if (issueService != null)
+                {
+                    var count = await issueService.GetIssueCount(Settings.Default.IssuesBeingTimeTrackedQuery);
+
+
+                    if (count < 1)
+                    {
+                        TaskbarIcon.ShowBalloonTip("You are burning daylight!", "Start some time tracker!",
+                            BalloonIcon.Warning);
+                    }
+                }
+                await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+
+        }
+
+        private void ResetYouTrackConnection()
+        {
+            var url = Settings.Default.YouTrackUrl;
+            var username = Settings.Default.YouTrackUsername;
+            var password = Settings.Default.YouTrackPassword;
+
+            if (!string.IsNullOrWhiteSpace(url) && !string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                _youTrackConnection = new UsernamePasswordConnection(url, username, password);
+            }
+            else
+            {
+                _youTrackConnection = null;
+            }
+        }
+
+        private void Open_SettingsWindow(object sender, RoutedEventArgs e)
+        {
+            var w = new SettingsWindow();
+            if (w.ShowDialog() == true)
+            {
+                ResetYouTrackConnection();
+            }
+        }
+    }
+}
---
+++ BurningDaylight/SettingsWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using BurningDaylight.Properties;
+
+namespace BurningDaylight
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Settings(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(YouTrackPasswordBox.Password))
+            {
+                Settings.Default.YouTrackPassword = YouTrackPasswordBox.Password;
+            }
+            Settings.Default.Save();
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Reload();
+            DialogResult = false;
+            Close();
+        }
+    }
+}
